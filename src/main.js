5b463b1c55108c8b6c3d5b57e7c9e9e79fac3711
lin-rm <lin_rongmei@126.com>
2019-07-08 19:31:39 +0800

    app.vue中完成首页

--- src/main.js
+++ src/main.js
@@ -3,6 +3,10 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
+import '../node_modules/bootstrap/dist/css/bootstrap.css'
+import './assets/css/index.css'
+
+
 new Vue({
   render: h => h(App),
 }).$mount('#app')
