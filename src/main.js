739f85d522954d005eda64157062bd5d34d9da7e
lin-rm <lin_rongmei@126.com>
2019-07-09 00:10:09 +0800

    实现武器、装备列表

--- src/main.js
+++ src/main.js
@@ -8,10 +8,14 @@ import './assets/css/index.css'
 import VueRouter from 'vue-router'
 Vue.use(VueRouter)
 import HeroList from './views/heroes/HeroList.vue'
+import WeaponList from './views/weapons/WeaponList.vue'
+import EquipList from './views/equips/EquipList.vue'
 const router = new VueRouter ({
   routes: [
     { name: 'home', path:'/', redirect:'/heroes'},
-    { name: 'heros', path:'/heroes', component: HeroList}
+    { name: 'heros', path:'/heroes', component: HeroList},
+    { name: 'weapons', path:'/weapons', component: WeaponList},
+    { name: 'equips', path:'/equips', component: EquipList}
   ]
 })
 
