f3b3c336194d14939896d85f5f0782f4f7e13894
lin-rm <lin_rongmei@126.com>
2019-07-09 00:15:40 +0800

    实现高亮显示

--- src/main.js
+++ src/main.js
@@ -11,6 +11,7 @@ import HeroList from './views/heroes/HeroList.vue'
 import WeaponList from './views/weapons/WeaponList.vue'
 import EquipList from './views/equips/EquipList.vue'
 const router = new VueRouter ({
+  linkExactActiveClass: 'active',
   routes: [
     { name: 'home', path:'/', redirect:'/heroes'},
     { name: 'heros', path:'/heroes', component: HeroList},
