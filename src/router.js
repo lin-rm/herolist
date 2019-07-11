8bcfb05de08f0fdf13b68e32c2ba7a0ff23e3985
lin-rm <lin_rongmei@126.com>
2019-07-11 17:57:12 +0800

    登录验证完成

--- src/router.js
+++ src/router.js
@@ -6,13 +6,16 @@ Vue.use(VueRouter)
 import HeroList from './views/heroes/HeroList.vue'
 import WeaponList from './views/weapons/WeaponList.vue'
 import EquipList from './views/equips/EquipList.vue'
+import HeroAdd from './views/heroes/HeroAdd.vue'
+
 const router = new VueRouter ({
   linkExactActiveClass: 'active',
   routes: [
     { name: 'home', path:'/', redirect:'/heroes'},
     { name: 'heros', path:'/heroes', component: HeroList},
     { name: 'weapons', path:'/weapons', component: WeaponList},
-    { name: 'equips', path:'/equips', component: EquipList}
+    { name: 'equips', path:'/equips', component: EquipList},
+    { name: 'heroesadd', path:' /heroes/add', component: HeroAdd},
   ]
 })
 
---
+++ src/views/heroes/HeroAdd.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
+    <h2 class="sub-header">添加英雄</h2>
+        <form>
+            <div class="form-group">
+                <label for="txtName">姓名</label>
+                <input type="text" class="form-control" v-model="formData.name" id="txtName" placeholder="姓名"/>
+            </div>
+            <div class="form-group">
+                <label for="sex">性别</label>
+                <select class="form-control" v-model="formData.gender">
+                <option value="男">男</option>
+                <option value="女">女</option>
+                </select>
+            </div>
+            <!-- prevent 阻止默认行为的执行 -->
+            <button @click.prevent="add" class="btn btn-success">提交</button>
+        </form>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
